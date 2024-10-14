3b9a445a3c78f7085418adb69e9711ccff36756f
code-walker-23 <chhabravinay549@gmail.com>
2024-10-14 12:01:27 +0530

    user-authentication-completed

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
-
+import { ClerkProvider } from "@clerk/nextjs";
 export const metadata = {
   title: "Schedullr",
   description: "Meeting Scheduling App",
@@ -11,20 +11,22 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        {/* header */}
-        <Header />
-        <main className="min-h-screen bg-gradient-to-b from-blue-50 to-white">
-          {children}
-        </main>
-        {/* footer */}
-        <footer className="bg-blue-100 py-12">
-          <div className="container mx-auto px-4 text-center text-gray-600">
-            <p>Made with ❤️ by Vinay Chhabra</p>
-          </div>
-        </footer>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          {/* header */}
+          <Header />
+          <main className="min-h-screen bg-gradient-to-b from-blue-50 to-white">
+            {children}
+          </main>
+          {/* footer */}
+          <footer className="bg-blue-100 py-12">
+            <div className="container mx-auto px-4 text-center text-gray-600">
+              <p>Made with ❤️ by Vinay Chhabra</p>
+            </div>
+          </footer>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
--- components/Header.jsx
+++ components/Header.jsx
@@ -3,10 +3,11 @@ import Link from "next/link";
 import React from "react";
 import { Button } from "./ui/button";
 import { PenBox } from "lucide-react";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 
 const Header = () => {
   return (
-    <nav className="mx-auto py-2 px-4 flex justify-between ">
+    <nav className="mx-auto py-2 px-4 flex justify-between items-center shadow-md border-b-2">
       <Link href={"/"} className="flex items-center">
         <Image
           alt="logo"
@@ -24,7 +25,14 @@ const Header = () => {
             Create Event
           </Button>
         </Link>
-        <Button variant="outline">Login</Button>
+        <SignedOut>
+          <SignInButton forceRedirectUrl="/dashboard">
+            <Button variant="outline">Login</Button>
+          </SignInButton>
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
       </div>
     </nav>
   );
