72f5acd6ed31bbc619f43ea9a04ebee8986f000b
code-walker-23 <chhabravinay549@gmail.com>
2024-10-15 18:55:40 +0530

    internal-navigation-via-routing

---
+++ app/not-found.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div className="text-4xl font-extrabold w-screen pt-80 grid justify-center">
+      404 - Page Not Found
+    </div>
+  );
+};
+
+export default NotFound;
