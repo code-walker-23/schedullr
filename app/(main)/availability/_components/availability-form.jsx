ba1ff51f7265b318c60edb05f95edae076842e4c
code-walker-23 <chhabravinay549@gmail.com>
2024-10-17 11:38:50 +0530

    fetching-availability

---
+++ app/(main)/availability/_components/availability-form.jsx
@@ -0,0 +1,7 @@
+"use client";
+const AvailabilityForm = ({ initialData }) => {
+  console.log(initialData);
+  return <div>AvailabilityForm</div>;
+};
+
+export default AvailabilityForm;
--- app/(main)/availability/page.jsx
+++ app/(main)/availability/page.jsx
@@ -1,7 +1,16 @@
+import { getuserAvailability } from "@/actions/availablity";
 import React from "react";
+import AvailabilityForm from "./_components/availability-form";
+import { defaultAvailability } from "./data";
 
-const Availability = () => {
-  return <div>Availability</div>;
+const Availability = async () => {
+  const availablity = await getuserAvailability();
+  console.log(availablity);
+  return (
+    <AvailabilityForm initialData={availablity || defaultAvailability}>
+      Availability
+    </AvailabilityForm>
+  );
 };
 
 export default Availability;
