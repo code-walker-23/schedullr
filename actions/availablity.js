69ac7c01dfb714bf80383948beca162ead432449
code-walker-23 <chhabravinay549@gmail.com>
2024-10-17 10:53:26 +0530

    fetching-user-availability

--- actions/availablity.js
+++ actions/availablity.js
@@ -32,5 +32,20 @@ export async function getuserAvailability() {
     "friday",
     "saturday",
     "sunday",
-  ];
+  ].forEach((day) => {
+    const foundDay = user.availability.days.find(
+      (data) => data.day === day.toUpperCase
+    );
+    availabilityData[day] = {
+      isAvailable: !!foundDay, // it converst into the boolean
+      startTime: foundDay
+        ? foundDay.startTime.toISOString().slice(11, 16)
+        : "09:00",
+      endTime: foundDay
+        ? foundDay.endTime.toISOString().slice(11, 16)
+        : "17:00",
+    };
+  });
+
+  return availabilityData;
 }
