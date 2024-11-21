90a437541ef7f9d838869ddbe523617d167f8006
code-walker-23 <chhabravinay549@gmail.com>
2024-11-22 00:13:48 +0530

    added dashboard and actions

--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -1,18 +1,22 @@
 "use client";
-import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
+
+import React, { useEffect } from "react";
 import { useUser } from "@clerk/nextjs";
-import React, { useEffect, useState } from "react";
-import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
-import { usernameSchema } from "@/app/lib/validator";
-import { useFetch } from "@/hooks/useFetch";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 import { updateUsername } from "@/actions/user";
 import { BarLoader } from "react-spinners";
+import { useFetch } from "@/hooks/useFetch";
+import { usernameSchema } from "@/app/lib/validator";
+import { getLatestUpdates } from "@/actions/dashboard";
+import { format } from "date-fns";
 
-const Dashboard = () => {
-  const { isLoaded, user } = useUser();
+export default function DashboardPage() {
+  const { user, isLoaded } = useUser();
+
   const {
     register,
     handleSubmit,
@@ -21,33 +25,66 @@ const Dashboard = () => {
   } = useForm({
     resolver: zodResolver(usernameSchema),
   });
-  const [origin, setOrigin] = useState("");
-  const { error, loading, fetchData } = useFetch(updateUsername);
 
   useEffect(() => {
-    if (isLoaded && user) {
-      setValue("username", user.username);
-    }
-  }, [isLoaded, user, setValue]);
+    setValue("username", user?.username);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isLoaded]);
+
+  const {
+    loading: loadingUpdates,
+    data: upcomingMeetings,
+    fetchData: fnUpdates,
+  } = useFetch(getLatestUpdates);
 
   useEffect(() => {
-    if (typeof window !== "undefined") {
-      setOrigin(window.location.origin);
-    }
+    (async () => await fnUpdates())();
   }, []);
 
+  const {
+    loading,
+    error,
+    fetchData: fnUpdateUsername,
+  } = useFetch(updateUsername);
+
   const onSubmit = async (data) => {
-    // Handle form submission logic
-    fetchData(data.username);
+    await fnUpdateUsername(data.username);
   };
 
   return (
     <div className="space-y-8">
       <Card>
         <CardHeader>
-          <CardTitle>Welcome, {user?.firstName}</CardTitle>
+          <CardTitle>Welcome, {user?.firstName}!</CardTitle>
         </CardHeader>
+        <CardContent>
+          {!loadingUpdates ? (
+            <div className="space-y-6 font-light">
+              <div>
+                {upcomingMeetings && upcomingMeetings?.length > 0 ? (
+                  <ul className="list-disc pl-5">
+                    {upcomingMeetings?.map((meeting) => (
+                      <li key={meeting.id}>
+                        {meeting.event.title} on{" "}
+                        {format(
+                          new Date(meeting.startTime),
+                          "MMM d, yyyy h:mm a"
+                        )}{" "}
+                        with {meeting.name}
+                      </li>
+                    ))}
+                  </ul>
+                ) : (
+                  <p>No upcoming meetings</p>
+                )}
+              </div>
+            </div>
+          ) : (
+            <p>Loading updates...</p>
+          )}
+        </CardContent>
       </Card>
+
       <Card>
         <CardHeader>
           <CardTitle>Your Unique Link</CardTitle>
@@ -56,7 +93,7 @@ const Dashboard = () => {
           <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
             <div>
               <div className="flex items-center gap-2">
-                <span>{origin}</span>
+                <span>{window?.location.origin}/</span>
                 <Input {...register("username")} placeholder="username" />
               </div>
               {errors.username && (
@@ -69,14 +106,14 @@ const Dashboard = () => {
               )}
             </div>
             {loading && (
-              <BarLoader className="mb-4" width={"100%"} color="36d7b7" />
+              <BarLoader className="mb-4" width={"100%"} color="#36d7b7" />
             )}
-            <Button type="submit">Update Username</Button>
+            <Button type="submit" disabled={loading}>
+              Update Username
+            </Button>
           </form>
         </CardContent>
       </Card>
     </div>
   );
-};
-
-export default Dashboard;
+}
